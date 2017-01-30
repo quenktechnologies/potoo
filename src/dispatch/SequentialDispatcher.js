cbd6098c387834a89baecf861f8698beafd80aab
Lasana Murray <lmurray@quenk.com>
2017-01-30 07:41:52 -0400

    refactor: force frame additions to be async.

--- src/dispatch/SequentialDispatcher.js
+++ src/dispatch/SequentialDispatcher.js
@@ -80,7 +80,7 @@ const ready = (messages, frames, self, root) =>
         type(Frame, frame =>
             (frames.push(frame),
                 root.tell(new ReceiveEvent({ name: frame.name, path: frame.context.path() })),
-                (gt0(messages, frames)) ? exec({ messages, frames, self, root }) : OK)),
+                (gt0(messages, frames)) ? setTimeout(exec({ messages, frames, self, root })) : OK)),
 
         type(Envelope, env =>
             (messages.push(env),
