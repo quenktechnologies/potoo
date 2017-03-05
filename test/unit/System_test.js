7d756344c6375791f10a14282001222718d97d7e
Lasana Murray <lmurray@quenk.com>
2017-03-05 14:02:22 -0400

    tests: System.exec

--- test/unit/System_test.js
+++ test/unit/System_test.js
@@ -1,65 +1,37 @@
 import must from 'must';
-import { Left, Right } from 'potoo-lib/monad';
-import { System, Spawn } from 'potoo-lib/System';
-import { ActorT } from 'potoo-lib/Actor';
+import { LocalT, Actor } from 'potoo-lib/Actor';
+import { System, exec } from 'potoo-lib/System';
+import { noop, spawn } from 'potoo-lib/Op';
 
-var sys;
+let sys;
+let actor;
+let r;
 
-class FakeT extends ActorT {}
-
-describe('System', function() {
+describe('exec', function() {
 
     beforeEach(function() {
 
         sys = new System();
+        actor = { path: '', parent: '' };
+        r = null;
 
     });
 
-    describe('spawn', function() {
+    it('should return a new System', function() {
 
-        it('should create a new Spawn task', function() {
-
-            let s = sys.spawn(new FakeT())
-            must(s.tasks[0]).be.instanceOf(Spawn);
-
-        });
+        must(exec(noop(), actor, sys)).be.instanceOf(System);
 
     });
 
-});
+    it('should run chained Ops', function() {
 
-describe('processSysQ', function() {
-
-    xit('should return Left if the system q is empty', function() {
-
-        let l = processSysQ(new System())
-
-        must(l).be.instanceOf(Left);
-        must(l.cata(s => s, s => s)).be.instanceOf(System);
-
-    });
-
-    xit('should return right if the system q has messages', function() {
-
-        let r = processSysQ(new System({}, { $: [new Kill()] }));
-
-        must(r).be.instanceOf(Right);
-        must(r.cata(s => s, s => s)).be.instanceOf(System);
-        must(r.cata(s => s, s => s).mailboxes.$.length).be(0);
-
-    });
-
-});
-
-describe('processUserQ', function() {
-
-    xit('should take the correct action', function() {
-
-        let a1 = new ActorT({ id: 'caw', start: x => x });
-        let r = processUserQ(new System({}, { $: [] }, [new Spawn({ parent: '', template: a1 })]));
+        let r = exec(
+            noop()
+            .chain(noop)
+            .chain(() => spawn(new LocalT({ id: 'a', start: () => {} }))), actor, sys);
 
         must(r).be.instanceOf(System);
-        must(r.actors.length).be(1);
+        must(r.a).be.instanceOf(Actor);
 
     });
 
