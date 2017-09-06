caa876c52db0d4855812bf4610db402e62fa1a00
Lasana Murray <lmurray@quenk.com>
2017-09-06 13:28:10 -0400

    fix: do not execute on non matches :/

--- src/Case.ts
+++ src/Case.ts
@@ -125,7 +125,6 @@ export class Case<T> {
                     return true;
                 } else {
 
-                    this._execute(m);
                     return false;
 
                 }
