7d198e657be4ff7743a3cddff407d0f491451299
Lasana Murray <lmurray@quenk.com>
2018-02-13 13:53:38 -0400

    refactor: rename confusing type param.

--- src/system/Envelope.ts
+++ src/system/Envelope.ts
@@ -3,10 +3,10 @@
  * Envelope for messages.
  * @param {Address} to The actor address the message is destined to.
  * @param {Address} from The actor that originally sent the message.
- * @param <P> message The message payload type.
+ * @param <M> message The message payload type.
  */
-export class Envelope<P> {
+export class Envelope<M> {
 
-    constructor(public to: string, public from: string, public message: P) { }
+    constructor(public to: string, public from: string, public message: M) { }
 
 }
