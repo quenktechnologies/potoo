b9443b01b74ac7773e1ade3b257229044e064922
Lasana Murray <lmurray@quenk.com>
2018-10-26 16:58:45 -0400

    refactor: update root

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import {ActorSystem, Configuration} from './system';
+import * as  log from './actor/system/log';
+import { rmerge } from '@quenk/noni/lib/data/record';
+import { System, ActorSystem, Configuration } from './actor/system';
 
-export const system = (conf?:Configuration)=> ActorSystem.create(conf);
+/**
+ * system creates a new actor system using the optionally passed
+ * configuration.
+ */
+export const system = (conf: Configuration = {}): System =>
+    new ActorSystem([], rmerge({
+        log: {
+            level: log.WARN,
+            logger: console
+        }
+    }, <any>conf));
