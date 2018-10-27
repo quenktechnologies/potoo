71cfbf43b2afb37a7a774a3b31a26e42083e0ed6
Lasana Murray <lmurray@quenk.com>
2018-10-27 13:55:00 -0400

    refactor(actor/system/op/spawn): use system allocate

--- src/actor/system/op/spawn.ts
+++ src/actor/system/op/spawn.ts
@@ -8,7 +8,7 @@ import {
 import { cons, noop } from '@quenk/noni/lib/data/function';
 import { Actor } from '../../';
 import { Template } from '../../template';
-import { newFrame } from '../state';
+import { Frame } from '../state/frame';
 import { Address, isRestricted, make } from '../../address';
 import { System } from '../';
 import { SystemError } from '../error';
@@ -51,7 +51,7 @@ export class Spawn extends Op {
 
     public level = log.INFO;
 
-    exec(s: System): void {
+    exec<F extends Frame>(s: System<F>): void {
 
         return execSpawn(s, this);
 
@@ -68,9 +68,9 @@ export class Spawn extends Op {
  * If that is successfull we create and check for a duplicate id
  * then finally add the child to the system.
  */
-export const execSpawn = (s: System, { parent, template }: Spawn) =>
+export const execSpawn = <F extends Frame>(s: System<F>, { parent, template }: Spawn) =>
     s
-        .actors
+        .state
         .getAddress(parent)
         .chain(path =>
             fromBoolean(!isRestricted(template.id))
@@ -91,28 +91,30 @@ export const execSpawn = (s: System, { parent, template }: Spawn) =>
 const makeAddress = (parent: Address) => (template: Template) =>
     fromString(make(parent, template.id))
 
-const checkAddress = (s: System, addr: Address) =>
-    fromBoolean(!s.actors.exists(addr));
+const checkAddress = <F extends Frame>(s: System<F>, addr: Address) =>
+    fromBoolean(!s.state.exists(addr));
 
-const generate = (s: System, template: Template) => (addr: Address) =>
-    fromNullable(newFrame(template.create(s), template))
-        .map(f => {
+const generate =
+    <F extends Frame>(s: System<F>, template: Template) => (addr: Address) =>
+        fromNullable(s.allocate(template))
+            .map(f => {
 
-            s.actors.put(addr, f);
+                s.state.put(addr, f);
 
-            s.exec(new Run(RUN_START_TAG, addr, template.delay || 0,
-                () => s.actors.runInstance(addr)));
+                s.exec(new Run(RUN_START_TAG, addr, template.delay || 0,
+                    () => s.state.runInstance(addr)));
 
-            return f.actor;
+                return f.actor;
 
-        });
+            });
 
-const spawnChildren = (s: System, t: Template) => (parent: Actor) =>
-    fromNullable(<Template[]>t.children)
-        .map(children => children.forEach(c => s.exec(new Spawn(parent, c))));
+const spawnChildren =
+    <F extends Frame>(s: System<F>, t: Template) => (parent: Actor) =>
+        fromNullable(<Template[]>t.children)
+            .map(children => children.forEach(c => s.exec(new Spawn(parent, c))));
 
 const raiseInvalidIdError =
-    (s: System, id: string, parent: Address) => () => {
+    <F extends Frame>(s: System<F>, id: string, parent: Address) => () => {
 
         s.exec(new Raise(new InvalidIdError(id), parent, parent));
         return nothing();
@@ -120,7 +122,7 @@ const raiseInvalidIdError =
     }
 
 const raiseDuplicateAddressError =
-    (s: System, parent: Address, addr: Address) => () => {
+    <F extends Frame>(s: System<F>, parent: Address, addr: Address) => () => {
 
         s.exec(new Raise(
             new DuplicateAddressError(addr), parent, parent));
