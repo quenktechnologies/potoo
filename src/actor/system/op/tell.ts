f47116e7cc5db0558b5cae0cbce00254f8ab3f16
Lasana Murray <lmurray@quenk.com>
2018-10-27 17:13:21 -0400

    refactor(actor/system/op/tell): update tell processing

--- src/actor/system/op/tell.ts
+++ src/actor/system/op/tell.ts
@@ -5,7 +5,8 @@ import { noop } from '@quenk/noni/lib/data/function';
 import { fromNullable } from '@quenk/noni/lib/data/maybe';
 import { Address } from '../../address';
 import { Message } from '../../message';
-import { Envelope } from '../mailbox';
+import { Mailbox, Envelope } from '../mailbox';
+import { Frame } from '../state/frame';
 import { System } from '../';
 import { Check } from './check';
 import { Transfer } from './transfer';
@@ -26,7 +27,7 @@ export class Tell extends Op {
 
     public level = log.INFO;
 
-    exec(s: System): void {
+    exec<F extends Frame>(s: System<F>): void {
 
         return execTell(s, this);
 
@@ -37,29 +38,58 @@ export class Tell extends Op {
 /**
  * execTell
  *
- * Puts a message in the destination actor's mailbox and schedules
- * the Check instruction if the destination still exists.
+ * If there is a router registered for the "to" address, the message
+ * is transfered.
+ *
+ * Otherwise provided, the actor exists, we put the message in it's
+ * mailbox and issue a Check.
  *
  * The message is dropped otherwise.
  */
-export const execTell = (s: System, { to, from, message }: Tell) =>
+export const execTell = <F extends Frame>(s: System<F>, op: Tell) =>
     s
-        .actors
-        .getRouter(to)
-        .map(r => s.exec(new Transfer(to, from, r, message)))
-        .orElse(() =>
-            s
-                .actors
-                .get(to)
-                .map(f =>
-                    tick(() => {
-                        f.mailbox.push(new Envelope(to, from, message));
-                        s.exec(new Check(to));
-                    }))
-                .orElse(() =>
-                    fromNullable(s.configuration.hooks)
-                        .chain((hs:hooks.Hooks) => fromNullable(hs.drop))
-                        .map((f: hooks.Drop) => f(new Envelope(to, from, message))))
-                .orJust(() => s.exec(new Drop(to, from, message))))
+        .state
+        .getRouter(op.to)
+        .map(runTransfer(s, op))
+        .orElse(runTell(s, op))
+        .orElse(invokeDropHook(s, op))
+        .orJust(justDrop(s, op))
         .map(noop)
         .get();
+
+const runTransfer = <F extends Frame>
+    (s: System<F>, { to, from, message }: Tell) => (r: Address) =>
+        s.exec(new Transfer(to, from, r, message));
+
+const runTell = <F extends Frame>(s: System<F>, op: Tell) => () =>
+    s
+        .state
+        .get(op.to)
+        .chain(doTell(s, op));
+
+const doTell = <F extends Frame>(s: System<F>, op: Tell) => (f: F) =>
+    f
+        .mailbox
+        .map(doTellMailbox(s, op))
+        .orJust(() => f.actor.accept(toEnvelope(op)));
+
+const doTellMailbox = <F extends Frame>
+    (s: System<F>, { to, from, message }: Tell) => (m: Mailbox) =>
+        tick(() => {
+
+            m.push(new Envelope(to, from, message));
+            s.exec(new Check(to));
+
+        });
+
+const invokeDropHook = <F extends Frame>(s: System<F>, op: Tell) => () =>
+    fromNullable(s.configuration.hooks)
+        .chain((h: hooks.Hooks) => fromNullable(h.drop))
+        .map((f: hooks.Drop) => f(toEnvelope(op)));
+
+const justDrop =
+    <F extends Frame>(s: System<F>, { to, from, message }: Tell) => () =>
+        s.exec(new Drop(to, from, message));
+
+const toEnvelope = ({ to, from, message }: Tell) =>
+    new Envelope(to, from, message);
