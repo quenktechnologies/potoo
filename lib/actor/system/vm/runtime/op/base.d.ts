dda31881dba67adc15974389bc9e3f75eb3dcee4
Lasana Murray <lmurray@quenk.com>
2020-04-07 21:37:59 -0400

    chore: rebuild

--- lib/actor/system/vm/runtime/op/base.d.ts
+++ lib/actor/system/vm/runtime/op/base.d.ts
@@ -103,3 +103,41 @@ export declare const call: (r: Runtime, f: Frame, n: number) => void;
  * <message> ->
  */
 export declare const raise: (r: Runtime, f: Frame, _: number) => void;
+/**
+ * jmp jumps to the instruction at the specified address.
+ *
+ * Stack:
+ *  ->
+ */
+export declare const jmp: (_: Runtime, f: Frame, args: number) => void;
+/**
+ * ifzjmp jumps to the instruction at the specified address if the top
+ * of the stack is === 0.
+ *
+ * Stack:
+ *
+ * <uint32> ->
+ */
+export declare const ifzjmp: (_: Runtime, f: Frame, args: number) => void;
+/**
+ * ifnzjmp jumps to the instruction at the specified address if the top
+ * of the stack is !== 0.
+ *
+ * Stack:
+ * <uint32> ->
+ */
+export declare const ifnzjmp: (_: Runtime, f: Frame, args: number) => void;
+/**
+ * ifeqjmp jumps to the instruction at the specified address if the top
+ * two elements of the stack are strictly equal to each other.
+ * Stack:
+ * <any><any> ->
+ */
+export declare const ifeqjmp: (r: Runtime, f: Frame, args: number) => void;
+/**
+ * ifneqjmp jumps to the instruction at the specified address if the top
+ * two elements of the stack are not strictly equal to each other.
+ * Stack:
+ * <any><any> ->
+ */
+export declare const ifneqjmp: (r: Runtime, f: Frame, args: number) => void;
